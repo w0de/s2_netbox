1ddf1a0b6eabfa289d3589674f9fab7a1aff015f
Harry Seeber <harry.seeber@gusto.com>
2018-01-23 12:29:54 -0700

    additional methods for person and credential classes

--- lib/s2_netbox/commands/credential.rb
+++ lib/s2_netbox/commands/credential.rb
@@ -2,4 +2,13 @@ class S2Netbox::Commands::Credential < S2Netbox::Commands::BaseCommand
   def self.object_name
     'Credential'
   end
+
+  def self.remove(person_id, card_format, card_id, attributes={}, access_levels=[], user_defined_fields=[], session_id=nil)
+    person_attributes = build_attributes(attributes, access_levels, user_defined_fields)
+    person_attributes['PERSONID'] = person_id
+    person_attributes['CARDFORMAT'] = card_format
+    person_attributes['CARDID'] = card_id
+
+    send_request("Remove#{object_name}", person_attributes, session_id)
+  end
 end
--- lib/s2_netbox/commands/person.rb
+++ lib/s2_netbox/commands/person.rb
@@ -2,4 +2,18 @@ class S2Netbox::Commands::Person < S2Netbox::Commands::BaseCommand
   def self.object_name
     'Person'
   end
+
+  def self.get(person_id, attributes={}, access_levels=[], user_defined_fields=[], session_id=nil)
+    person_attributes = build_attributes(attributes, access_levels, user_defined_fields)
+    person_attributes['PERSONID'] = person_id
+
+    send_request("Get#{object_name}", person_attributes, session_id)
+  end
+
+  def self.get_data(person_id, attributes={}, access_levels=[], user_defined_fields=[], session_id=nil)
+    person_attributes = build_attributes(attributes, access_levels, user_defined_fields)
+    person_attributes['PERSONID'] = person_id
+
+    send_request("SearchPersonData", person_attributes, session_id)
+  end
 end
