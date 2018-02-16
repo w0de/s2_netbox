4efff0c95fa2f7aa7efabf7ced05cc7c3f8c7216
Harry Seeber <harry.seeber@gusto.com>
2018-02-16 02:28:01 -0700

    SearchPersonData (poorly named) is actually a find person endpoint. Implemented a Person.find method. Removed incorrect Person.get_data method

--- lib/s2_netbox/commands/person.rb
+++ lib/s2_netbox/commands/person.rb
@@ -10,10 +10,14 @@ class S2Netbox::Commands::Person < S2Netbox::Commands::BaseCommand
     send_request("Get#{object_name}", person_attributes, session_id)
   end
 
-  def self.get_data(person_id, attributes={}, access_levels=[], user_defined_fields=[], session_id=nil)
+  # Find does not require a person_id
+  def self.find(person_id=nil, attributes={}, access_levels=[], user_defined_fields=[], session_id=nil)
     person_attributes = build_attributes(attributes, access_levels, user_defined_fields)
-    person_attributes['PERSONID'] = person_id
+    if person_id
+      person_attributes['PERSONID'] = person_id
+    end
 
     send_request("SearchPersonData", person_attributes, session_id)
   end
+
 end
